1aa354197fe55357914d0157688038cf2e98e5bf
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-08-24 15:29:46 -0400

    Updated

--- tests.nix
+++ tests.nix
@@ -14,16 +14,38 @@ makeTest {
 
         environment = {
           api_port = "3306";
+
+          db_database = "testPostAPI";
+          db_password = "VerySecurePassword";
+          db_username = "testuser";
+          db_host = "localhost";
         };
+
 
         extraDockerOptions = [ "--network=host" ];
       };
+    };
+
+    services.mysql = {
+      enable = true;
+      package = pkgs.mariadb;
+
+      initialScript = pkgs.writeText "mysql-init.sql" ''
+          CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'VerySecurePassword';
+          GRANT ALL PRIVILEGES ON testPostAPI.* TO 'testuser'@'localhost';
+      '';
+
+      initialDatabases = [
+        { 
+          name = "testPostAPI";
+        }
+      ];
     };
   };
 
   testScript = ''
     start_all()
 
-    client.succeed("curl localhost:3000")
+    client.wait_until_succeeds("curl localhost:3000")
   '';
 }
