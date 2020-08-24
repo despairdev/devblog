b94c9f2ce9f0b4953a32a365fcaa4b25eeb8c244
Julio Tain Sueiras <juliosueiras@gmail.com>
2020-08-24 15:20:13 -0400

    Updated

--- tests.nix
+++ tests.nix
@@ -5,9 +5,17 @@ makeTest {
 
   nodes = {
     client = { pkgs, lib, ... }: {
+
+      services.redis.enable = true;
+
       docker-containers.postAPI = {
         image = "devblog/postAPI:latest";
         imageFile = dockerImages.postAPI;
+
+        environment = {
+          api_port = "3306";
+        };
+
         extraDockerOptions = [ "--network=host" ];
       };
     };
@@ -15,5 +23,7 @@ makeTest {
 
   testScript = ''
     start_all()
+
+    client.succeed("curl localhost:3000")
   '';
 }
